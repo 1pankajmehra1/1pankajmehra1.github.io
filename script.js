ebf86f577580f0100409f0dbcfa483b663e67e4c
1pankajmehra1 <pankajbvoc@gmail.com>
2019-07-01 16:38:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+$(".circle1").on({
+    mouseenter: function() {
+     $(".paragraph").hide();
+     $(".para1").show();
+    }
+ });
+
+ $(".circle2").on({
+    mouseenter: function() {
+     $(".paragraph").hide();
+     $(".para2").show();
+    }
+ });
+
+ $(".circle3").on({
+    mouseenter: function() {
+     $(".paragraph").hide();
+     $(".para3").show();
+    }
+ });
+
+ $(".circle4").on({
+    mouseenter: function() {
+     $(".paragraph").hide();
+     $(".para4").show();
+    }
+ });
+
+ $(".circle5").on({
+    mouseenter: function() {
+     $(".paragraph").hide();
+     $(".para5").show();
+    }
+ });
+
+ $(".circle6").on({
+    mouseenter: function() {
+     $(".paragraph").hide();
+     $(".para6").show();
+    }
+ });
+ 
